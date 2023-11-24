74026b5bed8cd462536452f77b827970e6639000
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-24 14:14:30 +0900

    검색 결과 구현

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,4 +1,22 @@
+import { useLocation } from "react-router-dom";
 import "./Search.css";
+import { useFetch } from "../../hooks/useFetch";
+import { RecipeList } from "../../components/RecipeList";
 export default function Search() {
-  return <div>Search</div>;
+  //요청 주소의 쿼리스트링을 가져오는 방법
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q"); // q에 해당하는 값을 쿼리스트링에서 가져옴
+
+  const url = "http://localhost:3030/recipes?q=" + query;
+  const { data, isPending, error } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">"{query}"를 포함하는 레시피는 </h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">로딩중</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
