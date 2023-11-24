f4f5b3c73e204f06fdc2ae4f8b50c0d0f93d8ff9
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-24 10:47:54 +0900

    레시피 페이지 제목 표시

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,5 +1,16 @@
+import { useParams } from "react-router-dom";
 import "./Recipe.css";
+import { useFetch } from "../../hooks/useFetch";
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3030/recipes/" + id;
+  const { error, isPending, data: recipe } = useFetch(url);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">로딩중...</p>}
+      {recipe && <h2 className="page-title">{recipe.title}</h2>}
+    </div>
+  );
 }
