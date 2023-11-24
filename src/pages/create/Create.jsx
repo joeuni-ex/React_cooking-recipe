3e0a22721cd7f65331ab7c4bb665cf874859138d
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-24 11:47:24 +0900

    요리 재료 추가

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./Create.css";
 
 export default function Create() {
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState(""); //한 개의 재료
+  const [ingredients, setIngredients] = useState([]); // 모든 재료
+  const ingredientInput = useRef(); //특정 태그를 지정한다
 
+  //요리 재료 추가
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim(); //입력된 재료의 공백 제거
+
+    // 새 재료가 모든 재료에 없을 경우 모든 재료에 추가 한다.
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prev) => [...prev, newIngredient]); //이전 모든 재료에 추가한다.
+    }
+    setNewIngredient(""); //한 개의 재료는 공백으로
+    ingredientInput.current.focus(); //커서를 정해진 태그에 위치
+  };
+
+  //레시피 추가
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log(title, method, cookingTime, ingredients);
   };
 
   return (
@@ -25,7 +42,27 @@ export default function Create() {
           />
         </label>
 
-        {/* recipe ingredients here */}
+        {/*요리재료*/}
+        <label>
+          <span>요리 재료:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              사용
+            </button>
+          </div>
+        </label>
+        <p>
+          재료들 :{" "}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
 
         <label>
           <span>요리 방법:</span>
