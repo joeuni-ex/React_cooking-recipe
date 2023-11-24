30815465c7464215c23d01393e066ea0dc6728bf
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-24 10:38:43 +0900

    useFetch를 사용해서 레시피 가져오기

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,14 @@
+import { RecipeList } from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
 import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3030/recipes"); //서버 주소
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">로딩중</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
