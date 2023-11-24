1b5486ff74884d9809f7ed432e589d56c24658e6
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-24 14:00:06 +0900

    검색창 컴포넌트 추가 및 네브바에 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import SearchBar from "./SearchBar";
 
 export default function Navbar() {
   return (
@@ -8,6 +9,7 @@ export default function Navbar() {
         <Link to="/" className="brand">
           <h1>쿠킹 레시피</h1>
         </Link>
+        <SearchBar />
         <Link to="/create">레시피 만들기</Link>
       </nav>
     </div>
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./searchbar.css";
+
+export default function SearchBar() {
+  const [term, setTerm] = useState(""); //검색어
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${term}`); //q= 검색어로 요청한다.
+  };
+
+  return (
+    <div className="searchbar">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">검색:</label>
+        <input
+          type="text"
+          id="search"
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
