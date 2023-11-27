82cff9f316828daf289a4dfbcbe36ff32b01f6da
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-27 12:32:03 +0900

    onSnapshot 사용으로 자동 업데이트

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,32 +15,35 @@ export default function Home() {
   useEffect(() => {
     //파이어 스토어에서 데이터 가져오기
     setIsPending(true); //데이터 가져오기 작업시작(로딩중)
-    firedb
+    const unsub = firedb
       .collection("recipes") //컬렉션에서
-      .get() //가져온다.
-      .then((snapshot) => {
-        // console.log(snapshot.docs[0].data());
-        //가져온 데이터를 snapshot이라한다.
-        if (snapshot.empty) {
-          //데이터가 없을 때
-          setError("레시피가 없습니다."); //에러에 레시피 없음을 출력
-          setIsPending(false); //가져오기 종료
-        } else {
-          // 데이터가 있을 경우
-          let results = []; //빈 배열 생성
-          snapshot.docs.forEach((doc) => {
-            //반복문으로 결과를 배열에 넣는다.
-            results.push({ id: doc.id, ...doc.data() });
-          });
-          setData(results);
-          setIsPending(false); // 작업 종료
+      .onSnapshot(
+        (snapshot) => {
+          // console.log(snapshot.docs[0].data());
+          //가져온 데이터를 snapshot이라한다.
+          if (snapshot.empty) {
+            //데이터가 없을 때
+            setError("레시피가 없습니다."); //에러에 레시피 없음을 출력
+            setIsPending(false); //가져오기 종료
+          } else {
+            // 데이터가 있을 경우
+            let results = []; //빈 배열 생성
+            snapshot.docs.forEach((doc) => {
+              //반복문으로 결과를 배열에 넣는다.
+              results.push({ id: doc.id, ...doc.data() });
+            });
+            setData(results);
+            setIsPending(false); // 작업 종료
+          }
+        },
+        (error) => {
+          //에러 발생 시
+          setError(error.message); //에러 메세지 출력
+          setIsPending(false); //작업 종료
         }
-      })
-      .catch((err) => {
-        //에러 발생 시
-        setError(err.message); //에러 메세지 출력
-        setIsPending(false); //작업 종료
-      });
+      );
+
+    return () => unsub(); //useEffect가 종료될 때 즉, 다른 페이지로 이동했을 시
   }, []);
 
   return (
