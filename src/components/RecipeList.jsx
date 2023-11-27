06bd28e803d3f96c5b2cab232c26cd6d26f23d66
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-27 11:50:34 +0900

    firebase DB 삭제-  레시피 삭제

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,12 +2,20 @@ import React, { useContext } from "react";
 import "./RecipeList.css";
 import { Link } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
+import { firedb } from "../firebase/config";
+import Trashcan from "../assets/delete-icon.svg";
 
 export const RecipeList = ({ recipes }) => {
   const { mode } = useContext(ThemeContext);
   if (recipes.length === 0) {
     return <div className="error">검색된 레시피가 없습니다.</div>;
   }
+
+  //파이어베이스로 db삭제하기
+  const handleClick = (id) => {
+    firedb.collection("recipes").doc(id).delete();
+  };
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
@@ -16,6 +24,12 @@ export const RecipeList = ({ recipes }) => {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>요리하기</Link>
+          <img
+            src={Trashcan}
+            alt=""
+            className="delete"
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
