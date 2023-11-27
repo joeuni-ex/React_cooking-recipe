e07cb781363a04c872fe0edeed99a319b1004989
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-27 11:34:00 +0900

    firebase 연결 - 레시피 상세화면

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,14 +1,39 @@
 import { useParams } from "react-router-dom";
 import "./Recipe.css";
-import { useFetch } from "../../hooks/useFetch";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
+import { firedb } from "../../firebase/config";
 
 export default function Recipe() {
   const { mode } = useContext(ThemeContext);
   const { id } = useParams();
-  const url = "http://localhost:3030/recipes/" + id;
-  const { error, isPending, data: recipe } = useFetch(url);
+  // JSON 서버 DB
+  // const url = "http://localhost:3030/recipes/" + id;
+  // const { error, isPending, data: recipe } = useFetch(url);
+
+  // 파이어 베이스 DB
+  const [recipe, setRecipe] = useState(null); //한 개의 레시피 객체
+  const [isPending, setIsPending] = useState(false); //로딩 상태
+  const [error, setError] = useState(""); //에러메세지
+
+  useEffect(() => {
+    setIsPending(true);
+    firedb
+      .collection("recipes") //레시피 중
+      .doc(id) //id로
+      .get() //가져온다
+      .then((doc) => {
+        //console.log(doc.data()); //콘솔로 확인하기
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError("레시피를 찾을 수 없습니다.");
+        }
+      });
+  }, [id]);
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
