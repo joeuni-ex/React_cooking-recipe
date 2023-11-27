3b98ffc81c8bc5907995efd9829d9d4bd175f7ee
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-27 10:08:54 +0900

    레시피 리스트에 다크/라이트모드  추가

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./RecipeList.css";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../context/ThemeContext";
 
 export const RecipeList = ({ recipes }) => {
+  const { mode } = useContext(ThemeContext);
   if (recipes.length === 0) {
     return <div className="error">검색된 레시피가 없습니다.</div>;
   }
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="card">
+        <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
