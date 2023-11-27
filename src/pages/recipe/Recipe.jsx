85e02c96ca179b405630491be57d7694b1cf7614
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-27 10:09:01 +0900

    레시피 디테일에  다크/라이트모드  추가

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
 import "./Recipe.css";
 import { useFetch } from "../../hooks/useFetch";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 
 export default function Recipe() {
+  const { mode } = useContext(ThemeContext);
   const { id } = useParams();
   const url = "http://localhost:3030/recipes/" + id;
   const { error, isPending, data: recipe } = useFetch(url);
   return (
-    <div className="recipe">
+    <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">로딩중...</p>}
       {recipe && (
