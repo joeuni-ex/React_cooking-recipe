4889af5a40fb296006467d757e570f4757b839a9
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-27 14:02:26 +0900

    Searchbar이름변경

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Searchbar.css";
+import "./SearchBar.css";
 
 export default function SearchBar() {
   const [term, setTerm] = useState(""); //검색어
